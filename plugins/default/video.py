69d1291d0d8629b60274c2f0195812ab2681405a
pid <romkaeliseev@gmail.com>
2018-06-08 16:48:31 +0300

    БАНКААА

--- plugins/default/video.py
+++ plugins/default/video.py
@@ -1,11 +1,23 @@
 if answ[1] == 'видео':
-		param = (('v', '5.68'), ('q',answ_text),('count','10'),('access_token',token),('adult','0'),('forward_messages',torep))
+		param = (('v', '5.68'), ('q',answ_text),('count','10'),('access_token',token),('adult','1'),('forward_messages',torep))
 		res = requests.post('https://api.vk.com/method/video.search', data=param)
 		res = json.loads(res.text)
 		info = ''
 		if (res['response']['count'] != 0):
 			for item in res['response']['items']:
-				if item['id'] in (170754590,170631958,170067184,168063882,160911967,140239214,1609119666,166873080,166873080,166873080,166873080,166873080,166873080,166873080):
+				if item['id'] in (170754590,170631958,170067184,168063882,160911967,140239214,1609119666,166873080,166873080,166873080,166873080,166873080,166873080,166873080,160973038,166088674,166159461,456239165,159490950,169336720):
+					continue
+				if (item['title'].lower().find('банк') != -1) and (item['title'].lower().find('жоп') != -1):
+					continue
+				if (item['title'].lower().find('банк') != -1) and (item['title'].lower().find('сел') != -1):
+					continue
+				if (item['title'].lower().find('банк') != -1) and (item['title'].lower().find('мужик') != -1):
+					continue
+				if (item['title'].lower().find('мужик') != -1) and (item['title'].lower().find('сел') != -1):
+					continue
+				if (item['title'].lower().find('guy') != -1) and (item['title'].lower().find('cup') != -1):
+					continue
+				if (item['title'].lower().find('cup') != -1) and (item['title'].lower().find('guy') != -1):
 					continue
 				if item['title'].lower().find('theync') != -1:
 					continue
