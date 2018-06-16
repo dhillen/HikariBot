bab19361404ce7b6f6904b444b614e7a6fd49d70
pid <romkaeliseev@gmail.com>
2018-06-16 23:45:46 +0300

    цитата

--- plugins/default/quote.py
+++ plugins/default/quote.py
@@ -13,28 +13,28 @@ if answ[1] == 'цитата':
 		text += resnew['response']['items'][0]['fwd_messages'][k]['body']+'\n'
 	text = text.replace('\n','<br>')
 	url = ret['response'][0]['photo_max']
-	name = ret['response'][0]['first_name']+' '+ret['response'][0]['last_name']
+	name = ret['response'][0]['first_name']+' '+ret['response'][0]['last_name'] + ':'
 	avatar = requests.get(url, stream=True).raw
 	#####
 	avatar = Image.open(avatar)
-	avatar = avatar.resize([50,50],resample=Image.BILINEAR)
+	avatar = avatar.resize([180,180],resample=Image.BILINEAR)
 	data = textwrap.wrap(text, 50)
 	data = '<br>'.join(data)
 	data = data.split('<br>')
-	y = 50+(29*len(data))
-	fnt = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf', 25)
-	title = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 30)
-	img = Image.new('RGB', (750, y), color = (255,255,255))
+	y = 270+(40*len(data))
+	fnt = ImageFont.truetype('/home/pid/lera_bot/files/fonts/font.ttf', 37)
+	title = ImageFont.truetype('/home/pid/lera_bot/files/fonts/font1.ttf', 60)
+	img = Image.new('RGB', (1050, y), color = (255,255,255))
 	d = ImageDraw.Draw(img)
-	d.rectangle([0,0,750,50],outline=None,fill=(211,211,211))
-	img.paste(avatar,[0,0,50,50])
+	d.rectangle([0,0,750,150],outline=None,fill=(255,255,255))
+	img.paste(avatar,[0,0,180,180])
 	#d.text((0,50), '\n'.join(data), font=fnt, fill=(0, 0, 0))
-	offset = 50
+	offset = 210
 	for line in range(len(data)):
 		#print(str(line)+':'+data[line])
-		d.text((0,offset),data[line],font=fnt,fill=(0,0,0))
-		offset += 29
+		d.text((40,offset),data[line],font=fnt,fill=(0,0,0))
+		offset += 50
 		#print(offset)
-	d.text((55,5), name, font=title, fill=(0, 0, 0))
+	d.text((350, 60), name, font=title, fill=(0, 0, 0))
 	img.save('tmp/quote.jpg')
 	sendpic('quote.jpg','',toho,'')
